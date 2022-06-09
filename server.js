b57030eecfedd92c2fd19aed42999ececcfabddf
Justin Ellis <jdellis490@gmail.com>
2022-06-08 22:20:23 -0700

    Added case/break and future user command functions

--- server.js
+++ server.js
@@ -1,6 +1,48 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
 const consoleTable = require('console.table');
-const connection = require('./config/connection');
+const dataBase = require('./config/connection');
 const questions = require('./lib/questions');
 
+const startUp = () => {
+    inquirer.prompt(questions.beginQuestions)
+    .then((answers) => {
+
+        switch (answers.nextOption) {
+            case 'View Departments':
+                viewDepts();
+                break;
+            
+            case 'View Roles':
+                viewRoles();
+                break;
+
+            case 'View Employees':
+                viewEmployees();
+                break;
+            
+            case 'Add Departments':
+                addDepts();
+                break;
+            
+            case 'Add Roles':
+                addRoles();
+                break;
+
+            case 'Add Employees':
+                addEmployees();
+                break;
+
+            case 'Update Employee Roles':
+                updateCurrentRoles();
+                break;
+
+            case 'Exit':
+                exitSystem();
+                break;
+        }
+    })
+    .catch(err => {
+        if (err) throw err;
+    });
+};
